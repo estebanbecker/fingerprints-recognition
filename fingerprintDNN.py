db53d9dfe3d28f96403163e4d049cb7e94ed8c63
Esteban Becker <git@beckeresteban.eu>
2022-04-03 19:44:03 +0200

    End

--- fingerprintDNN.py
+++ fingerprintDNN.py
@@ -13,7 +13,7 @@ import time
 from os import listdir
 import os
 import copy
-import cv2
+import PIL
 
 model_conv = torch.load('model_conv.pt')
 
@@ -25,18 +25,26 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 data_dir='fvc2000_final_test'
 
+class_names = [x for x in range(0,10)]
+
+score=0
 
 for img in listdir(data_dir):
     img_path = os.path.join(data_dir, img)
-    img = cv2.imread(img_path)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    img = cv2.resize(img, (224, 224))
-    img = np.array(img)
-    img = img.transpose((2, 0, 1))
-    img = img.reshape(1, 3, 224, 224)
-    img = torch.from_numpy(img)
+    img = PIL.Image.open(img_path)
+    img = img.convert('RGB')
+    img = transforms.Resize(256)(img)
+    img = transforms.CenterCrop(224)(img)
+    img = transforms.ToTensor()(img)
+    img = transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])(img)
     img = img.float()
+    img = img.unsqueeze(0)
     img = img.to(device)
     outputs = model_conv(img)
-    _, predicted = torch.max(outputs.data, 1)
-    print(predicted + outputs.data)
+    _, predicted = torch.max(outputs, 1)
+
+    if(class_names[predicted.item()]==int(img_path[-5])):
+        score+=1
+    print("Predicted: ", class_names[predicted.item()], "Actual: ", img_path[-5])
+print("Accuracy:",score/len(listdir(data_dir)))
+    
